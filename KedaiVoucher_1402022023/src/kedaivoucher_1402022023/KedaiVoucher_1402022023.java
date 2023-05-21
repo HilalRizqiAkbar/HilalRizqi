868fc41bfce31109ef5a9db44c5c257a54cbb5b2
HilalRizqiAkbar <132528950+HilalRizqiAkbar@users.noreply.github.com>
2023-05-21 07:33:10 +0700

    Add files via upload

---
+++ KedaiVoucher_1402022023/src/kedaivoucher_1402022023/KedaiVoucher_1402022023.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kedaivoucher_1402022023;
+
+/**
+ *
+ * @author Hilal R Akbar
+ */
+public class KedaiVoucher_1402022023 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
